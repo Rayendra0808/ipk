a7383b76aa368152d6c922cbec23bf1ed60ee42e
Kukuh Prabowo <kukuuhhh@gmail.com>
2022-03-02 16:46:19 +0700

    map: use jqvmap for map ui

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -36,8 +36,7 @@
   <link rel="stylesheet" href="{{asset('assets/lib/owl/assets/owl.carousel.min.css')}}">
   <link rel="stylesheet" href="{{asset('assets/lib/owl/assets/owl.theme.default.min.css')}}">
   <link rel="stylesheet" href="{{asset('assets/lib/aos/aos.css')}}">
-  <link rel="stylesheet" href="{{asset('assets/lib/leaflet/leaflet.css')}}">
-  <script src="{{asset('assets/lib/leaflet/leaflet.js')}}"></script>
+  <link rel="stylesheet" href="{{asset('assets/jqvmap/jqvmap.css')}}">
 </head>
 
 <body>
@@ -168,16 +167,6 @@
       });
 
     });
-
-    const map = L.map('map').setView([-2.548926, 118.0148634], 5);
-    const tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-      maxZoom: 20,
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      id: 'mapbox/streets-v11',
-      tileSize: 512,
-      zoomOffset: -1
-    }).addTo(map);
 
     // create initial empty chart
     const ctx_live = document.getElementById("profil-ipk-nasional");
