ea9f3db6077035157fba975bd4ce3e973d9c2a9f
Irsyad Abdul <abdulirsyad15@gmail.com>
2023-10-20 17:01:43 +0700

    rename name file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -55,9 +55,9 @@
                                         </div>
                                         <div class="modal-body">
                                             <a target="_blank" href="{{ asset('assets/img') }}/handbook_ipk.pdf"
-                                                class="btn btn-download">@include('icons/pdf-icon') Handbook IPK 2020</a>
-                                            <a target="_blank" href="{{ asset('assets/pdf') }}/Book IPK 2023 Update 18 Okt 2023.pdf"
-                                                class="btn btn-download">@include('icons/pdf-icon') Handbook IPK 2023</a>
+                                                class="btn btn-download">@include('icons/pdf-icon') Handbook IPK 2018</a>
+                                            <a target="_blank" href="{{ asset('assets/pdf') }}/IPK 2022 dan Analisis Komparatif IPK.pdf"
+                                                class="btn btn-download">@include('icons/pdf-icon') IPK 2022 dan Analisis Komparatif IPK</a>
                                         </div>
                                     </div>
                                 </div>
